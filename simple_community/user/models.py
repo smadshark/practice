78f21ebffcee5af1857403c654611e38c11c7c68
smadshark <smadshark@gmail.com>
2021-02-26 02:59:46 +0900

    회원가입

--- simple_community/user/models.py
+++ simple_community/user/models.py
@@ -8,5 +8,10 @@ class User(models.Model):
     user_pw = models.CharField(max_length=64, verbose_name='사용자 비밀번호')
     reg_at = models.DateTimeField(auto_now_add=True, verbose_name='등록 일시')
 
+    def __str__(self):
+        return self.username
+
     class Meta:
         db_table = "user_info"
+        verbose_name = "심플커뮤니티 사용자"
+        verbose_name_plural = "심플커뮤니티 사용자"
--- simple_community/user/views.py
+++ simple_community/user/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.hashers import make_password
+from .models import User
+
 
 # Create your views here.
+
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    elif request.method == 'POST':
+        username = request.POST['username']
+        user_pw = request.POST['user-pw']
+        check_pw = request.POST['check-pw']
+
+        res_data = {}
+        if user_pw != check_pw:
+            res_data['error'] = '비밀번호가 다릅니다.'
+        else:
+            user = User(
+                username=username,
+                user_pw=make_password(user_pw)
+            )
+
+            user.save()
+
+        return render(request, 'register.html', res_data)
